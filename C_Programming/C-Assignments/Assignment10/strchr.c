ae329d1cf7eeecaf11daac7d7463e7207f67fcec
rutuja-bhagat01 <rutujabhagat0901@gmail.com>
2025-09-28 21:30:07 +0530

    C Programming Tests and Project
    Uploading C Programming Tests and Project

---
+++ C_Programming/C-Assignments/Assignment10/strchr.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include <string.h>
+void main(){
+			
+	// 8.strchr() = find first occ. of given char, in string
+	char str[]= "Hello, World!!!";
+	
+	//finding 1st occ of 'o'
+		if(res!=NULL)
+	else
+		printf("Character not found");
+	++;                          
